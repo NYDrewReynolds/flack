430c792eb3c004783540bb692f32baecc5c677f7
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-17 15:44:31 -0600

    Add Messages & Indexes

--- db/migrate/20150917212407_create_messages.rb
+++ db/migrate/20150917212407_create_messages.rb
@@ -1,8 +1,9 @@
-class CreateUserRooms < ActiveRecord::Migration
+class CreateMessages < ActiveRecord::Migration
   def change
-    create_table :user_rooms do |t|
+    create_table :messages do |t|
       t.references :user, index: true, foreign_key: true
       t.references :room, index: true, foreign_key: true
+      t.string  :body
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917211225) do
+ActiveRecord::Schema.define(version: 20150917212407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "messages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "room_id"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "messages", ["room_id"], name: "index_messages_on_room_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
   create_table "rooms", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -28,9 +40,21 @@ ActiveRecord::Schema.define(version: 20150917211225) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "user_rooms", ["room_id"], name: "index_user_rooms_on_room_id", using: :btree
+  add_index "user_rooms", ["user_id"], name: "index_user_rooms_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "screen_name"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.string   "oauth_token_secret"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "user_rooms", "rooms"
+  add_foreign_key "user_rooms", "users"
 end
