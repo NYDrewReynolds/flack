6485484e29848b32b12c2f8c869d1fe569e7498d
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-27 14:29:43 -0600

    Refactor JS, messages controller, & Rooms show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,20 @@
 class MessagesController < ApplicationController
-  require('byebug')
 
   def create
-    message_body = params[:body]
-    room_id = params[:room_id]
-    room = Room.find(room_id)
-    user_id = params[:user_id]
-    screen_name = User.find(user_id).screen_name
-    Message.create(body: message_body, room_id: room_id, user_id: user_id)
-    respond_to do |format|
-      format.js { render json: {success: true} }
-      format.json { render json: {success: true, body: params[:body], screen_name: screen_name} }
+    message = Message.new(message_params)
+    if message.valid?
+      message.save
+      render partial: "shared/message", locals: {message: message}
+    else
+      flash.now[:errors] = "Invalid message!"
     end
-    REDIS.publish("flack:#{room.name}", message_body)
+    REDIS.publish("flack:#{message.room.name}", message.body)
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :room_id, :body)
+  end
+
 end
